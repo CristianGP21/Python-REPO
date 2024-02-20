467aa127b2993959c1d1c0e42fb0784de173cef0
CristianGP21 <157290204+CristianGP21@users.noreply.github.com>
2024-02-20 13:03:39 +0200

    Add files via upload

---
+++ Pong Game/Paddle.py
@@ -0,0 +1,26 @@
+import pygame
+
+WHITE = (255, 255, 255)
+
+class Paddle:
+    COLOR = WHITE
+    VELOCITY = 4
+
+    def __init__(self, x, y, width, height):
+        self.x = self.original_x = x
+        self.y = self.original_y = y
+        self.width = width
+        self.height = height
+
+    def draw(self, window):
+        pygame.draw.rect(window, self.COLOR, (self.x, self.y, self.width, self.height))
+
+    def move(self, up=True):
+        if up:
+            self.y -= self.VELOCITY
+        else:
+            self.y += self.VELOCITY
+
+    def reset(self):
+        self.x = self.original_x
+        self.y = self.original_y
